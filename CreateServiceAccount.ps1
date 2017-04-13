8dafc25a13634444d951fbec9fd7e9582048ea80
sampka <sampka@gmail.com>
2017-04-13 17:33:04 -0400

    first

---
+++ CreateServiceAccount.ps1
@@ -0,0 +1,296 @@
+﻿param([string] $Type = "ErrorEnviroment", [string] $NewUser = "ErrorSite")
+
+
+function errorCheck {
+    param($result)
+    if($result.Errors.length -gt 0){
+        "The following errors were received:"
+        $result.Errors[0]
+        exit
+    }
+}
+
+function findFieldId {
+    param($template, [string]$name)
+    $template.Fields | ForEach-Object {
+      
+        if ($_.DisplayName -eq $name) {
+            $fieldid = $_.Id
+           # write-host $_.DisplayName
+            return $_.Id
+        }
+    }
+    if ($fieldid -eq $null) {
+        Write-Host "No matching field ID was found."
+        exit
+    }
+}
+
+function findTemplate {
+    param($templateType)
+   
+    $result_temp = $proxy.GetSecretTemplates($token)
+   
+    errorCheck $result_temp
+    $templates = $result_temp.SecretTemplates
+
+    $templates | ForEach-Object {
+    
+    #write-host $_.Name
+
+        if($_.Name -eq $templateType){
+            return $_
+        }
+    }
+    if ($templates.length -lt 1) {
+        Write-Host "No matching Secret template was found."
+        exit
+    }
+}
+
+function findFolderId {
+    param($folderName)
+    $result_folder = $proxy.SearchFolders($token, $folderName)
+    
+  #  write-host $result_folder.Folders.TypeId
+    
+    errorCheck $result_folder
+    return $result_folder.Folders[0].Id
+}
+
+function CreateNewSecret {
+    param($token, $newFolder, $newTemplate, $newDomain, $newUser, $newPassword, $folderId, $mycreds, $newType, $internalUser, $enviroment, $intusername, $inttextpassword)
+
+  
+
+
+    
+    $template = findTemplate $newTemplate
+
+    # if no password is provided, generate a new password
+    if($newPassword.length -lt 1)
+    {
+       # Write-Host "Password being Generated..."
+        $pwdId = (findFieldId $template "Password")
+        $result_pwd = $proxy.GeneratePassword($token, $pwdId)
+        errorCheck $result_pwd
+        $newPassword = $result_pwd.GeneratedPassword
+       # write-host $newPassword
+   
+    }
+
+    # ensure you are including ALL Secret fields here, even if they are empty
+    $secretItemFields = ((findFieldId $template "Domain"), (findFieldId $template "Username"), (findFieldId $template "Password"), (findFieldId $template "Notes"))
+    
+    $secretItemValues = ($newDomain, $newUser, $newPassword, "")
+
+   
+    
+    $securepassword = ConvertTo-SecureString $newPassword -AsPlainText -Force
+   
+    
+    $secretName = $newUser
+
+
+    try
+    {
+    
+    new-aduser -name "$newUser" -SamAccountName "$newUser" -GivenName "$newUser" -DisplayName "$newUser" -UserPrincipalName ("$newUser" + '@mi.corp.rockfin.com') -AccountPassword $securepassword -changepasswordatlogon $false -passwordneverexpires $true -Path 'OU=Service,DC=mi,DC=corp,DC=rockfin,DC=com' -enabled $true -Credential $mycreds
+
+    $result_add = $proxy.AddSecret($token, $template.Id, $secretName, $secretItemFields, $secretItemValues, $folderId)
+    
+    errorCheck $result_add
+    
+    Write-Host "Secret $secretName has been created."
+
+    $updateSecret = $result_add.secret
+
+    
+    }
+    catch
+    {
+    write-host "Account Creation failed :("
+    }
+
+   # $argumentlist = '-Type '+ $newType,'-SiteName '+$internalUser,'-AppPooluser '+$newUser,'-AppPoolPassword "'+$newPassword+'"','-EnviromentTier 1'
+
+    $quotepassword = "'+$newPassword+'"
+
+
+   Start-Process powershell.exe -ArgumentList '-NoExit ', '.\NewWebService.ps1', $newType, $internalUser, $newUser, $quotepassword, $enviroment, $intusername, $inttextpassword
+
+
+  # \\MI\DFS\USERS\SKaufman\Desktop\Scripts\PowerShell\SuperSiteCreationScripts\NewWebService.ps1 $argumentlist
+
+    
+
+
+   <# 
+    Write-Host "`nEnabling Requires Approval for Access..."
+    
+    # set IsChangeToSettings to be true to put changes in effect
+    $updateSecret.SecretSettings.IsChangeToSettings = 1
+    
+    # enable request approval and specify approver(s)
+    $updateSecret.SecretSettings.RequiresApprovalForAccess = 1
+
+    $type = $proxy.GetType().GetMethod("UpdateSecretPermission").GetParameters()[2].ParameterType.FullName
+    
+    $userRecord1 = New-Object -TypeName $type
+    $userRecord2 = New-Object -TypeName $type
+
+    $userRecord1.UserId = 3
+    $userRecord1.GroupId = $null
+    $userRecord1.IsUser = $true
+    $userRecord2.UserId = $null
+    $userRecord2.GroupId = 10
+    $userRecord2.IsUser = $false
+    
+    $updateSecret.SecretSettings.Approvers = @($userRecord1, $userRecord2)
+    
+    $result_update = $proxy.UpdateSecret($token, $updateSecret)
+    errorCheck $result_update
+    
+    Write-Host "Require Approval enabled.`n"
+    #>
+  
+    return $newPassword
+
+}
+
+
+if($Type -eq "ErrorEnviroment")
+{
+
+$Type = Read-Host "What Server group is this for?"
+
+
+}
+
+
+# provide new account information, including the destination folder and template type
+
+
+switch($Type)
+{
+ Midware
+    {
+        $newFolder = 'Prod Domain Applications'
+        break;
+    }
+ Intweb
+    {
+        $newFolder = 'Internal Applications'
+        break;
+
+    }
+  External45iis
+  {
+       $newFolder = 'Prod Domain Applications'
+        break;
+  }
+  QLMidware
+  {
+   $newFolder = 'Prod Domain Applications'
+        break;
+  }
+  default
+  {
+  write-host "Invalid Server group"
+  break;
+  }
+ 
+}
+
+
+
+$newTemplate = 'Active Directory Account'
+$newDomain = 'mi'
+
+# leave password blank to generate a new one
+$newPassword = ''
+
+
+
+  # login info
+    $url = 'https://secretserver/webservices/sswebservice.asmx'
+    $username = read-host "Enter your Secret Server username"
+    $password = read-host "Password" -AsSecureString
+    $domain = 'mi'   # leave blank for local users
+    $proxy = New-WebServiceProxy -uri $url -UseDefaultCredential
+
+    # authenticate to Secret Server
+    Write-Host "`nAuthenticating..."
+    $BSTR = `
+    [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($password)
+    $result_auth = $proxy.Authenticate($username, [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR), '',$domain)
+    errorCheck $result_auth
+    Write-Host "Authentication Successful."
+
+
+
+    # obtain token
+    $token = $result_auth.Token
+
+  
+ 
+
+$newUser = Read-host 'Site Name'
+
+
+
+#create users
+######
+$clean = [boolean] 1
+
+$searchUser = "User Not Found"
+$NewUsers = new-object String[] 3
+$NewUsers[0] = "ap-" + $newUser + "T"
+$NewUsers[1] = "ap-" + $newUser + "B"
+$NewUsers[2] = "ap-" + $newUser
+$ADComputer = "ql1dc2.mi.corp.rockfin.com"
+$ADDomain = [ADSI]"WinNT://$ADComputer"
+$ADDomain =[ADSI]"LDAP://ldapquery.rockfin.com"
+
+$mycreds = New-Object System.Management.Automation.PSCredential ($username, $password)
+
+$textpassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
+
+
+for($index=0; $index -lt 3; $index++)
+{
+    try
+    {
+        $searchUser = Get-ADUser -Identity $NewUsers[$index] | Out-Null
+    }
+    catch
+    {
+        $searchUser = "User Doesnt Exsist"
+        
+    }
+    
+    if($searchUser -eq $null)
+    {
+        write-host "Error $NewUsers[$index] already exists"
+        $clean = [boolean] 0
+        
+    }
+}
+
+
+
+
+if($clean)
+{
+$folderIdroot = findFolderId $newFolder
+$Folder_add = $proxy.FolderCreate($token, $newUser, $folderIdroot, '1')
+$folderId = findFolderId $newUser
+for($index=0; $index -lt 3; $index++)
+    {
+    
+       $test = CreateNewSecret $token $newFolder $newTemplate $newDomain $NewUsers[$index] $newPassword $folderId $mycreds $Type $NewUser $index $username $textpassword
+       
+       write-host "Password: $test"
+       write-host ''
+    }
+}
---
+++ NewWebService.ps1
@@ -0,0 +1,217 @@
+﻿param([string] $Type = "Midware", [string]$SiteName = "ErrorSite", [string] $AppPoolUser = "ErrorUser",[string] $AppPoolPassword = " ",[string] $EnviromentTier = " ", [string] $username = " ", [string] $password = " ")
+
+
+$SecurePassword = $password | ConvertTo-SecureString -AsPlainText -Force
+
+$mycreds = New-Object System.Management.Automation.PSCredential ($username, $SecurePassword)
+
+
+if($SiteName -eq "ErrorSite")
+{
+$SiteName = Read-Host 'Site Name? '
+}
+
+if($AppPoolUser -eq "ErrorUser")
+{
+$AppPoolUser = Read-Host 'Service Account Name? '
+}
+
+if($AppPoolPassword -eq " ")
+{
+$AppPoolPassword = Read-Host 'Service Account Password? '
+}
+ 
+if($EnviromentTier -eq " ")
+{
+$EnviromentTier = Read-Host '0. Test, 1. Beta, 2. Prod '
+}
+
+
+
+
+switch($Type)
+{
+    QLMidware
+    {
+        $serverTest = "ql2mwtest1"
+        $serverBeta = "ql1mwbeta1", "ql2mwbeta1"
+        $serverProd = "ql1mw1", "ql1mw2", "ql1mw3", "ql1mw4", "ql1mw5", "ql2mw1", "ql2mw2", "ql2mw3", "ql2mw4", "ql2mw5"
+
+
+        $domain = "mi.corp.rockfin.com"
+        break;
+    }
+    DNWeb
+    {
+       $serverTest = 
+        break;
+    }
+    Midware
+    {
+        $serverTest = "test1midware1"
+        $serverBeta = "Beta1midware1", "Beta2midware1"
+        $serverProd = "Prod1midware1", "Prod1midware2", "Prod1midware3", "Prod1midware4", "Prod2midware1", "Prod2midware2", "Prod2midware3", "Prod2midware4"
+
+
+      
+        break;
+    }
+    IntWeb
+    {
+        $serverTest = "ql2intwebtest1"
+        $serverBeta = "ql2intwebbeta1"
+        $serverProd = "ql2intweb1", "ql1intweb1"
+    break;
+
+    }
+    External45iis
+    {
+        $serverTest = "test1n45iis1"
+        $serverBeta = "beta1n45iis1"
+        $serverProd = "prod1n45iis1", "prod2n45iis1"
+
+    break;
+
+    }
+    Default
+    {
+        Write-Host -ForegroundColor red "Invalid Type.  Exiting script."
+        exit;
+    }
+}
+
+if($SiteName -eq "ErrorSite")
+{
+    Write-Host -ForegroundColor red "Invalid Site.  Exiting script."
+    exit;
+}
+
+
+
+#connect and configure site and app pools
+
+    switch($EnviromentTier)
+    {
+        0 #Test
+        {
+           $ServerList = $ServerTest
+           Write-Host -ForegroundColor green "Connecting to Test Servers"
+           
+        }
+        1 #Beta
+        {
+            $ServerList = $ServerBeta
+            Write-Host -ForegroundColor green "Connecting to Beta Servers"
+         
+        }
+        2 #Prod
+        {
+           $ServerList = $ServerProd
+           Write-Host -ForegroundColor green "Connecting to Prod Servers"
+           
+        }
+    }
+    
+    foreach($server in $ServerList)
+    {
+
+
+        
+        $FQDN = $Server 
+        Write-Host -ForegroundColor green "Creating session for $server..."
+        $Session = new-pssession -computername $FQDN -credential $mycreds
+
+   
+
+        switch($EnviromentTier)
+        {
+            1 #Test
+            {
+                 # set variables in remote session
+                Write-Host -ForegroundColor green "Setting environment in remote server Test..."
+                invoke-command -session $Session -scriptblock{
+                    param($innerNewSiteName, $innerAppPoolUser, $innerAppPoolPassword)
+                    $NewUser = "ap-" + $innerNewSiteName + "T"
+                    $NewSite = $innerNewSiteName + "Test"
+                    $SecurePassword = "Password1"
+                } -ArgumentList $SiteName, $AppPoolUser, $AppPoolPassword               
+          
+               
+            }
+            2 #Beta
+            {
+                # set variables in remote session
+                Write-Host -ForegroundColor green "Setting environment in remote server Beta..."
+                invoke-command -session $Session -scriptblock{
+                    param($innerNewSiteName, $innerAppPoolUser, $innerAppPoolPassword)
+                    $NewUser = "ap-" + $innerNewSiteName + "B"
+                    $NewSite = $innerNewSiteName + "Beta"
+                    $SecurePassword = "Password1"
+                } -ArgumentList $SiteName, $AppPoolUser, $AppPoolPassword               
+              
+            }
+            3 #Prod
+            {
+                   # set variables in remote session
+                Write-Host -ForegroundColor green "Setting environment in remote server Prod..."
+                invoke-command -session $Session -scriptblock{
+                    param($innerNewSiteName, $innerAppPoolUser, $innerAppPoolPassword)
+                    $NewUser = "ap-" + $innerNewSiteName
+                    $NewSite = $innerNewSiteName
+                    $SecurePassword = "Password1"
+                } -ArgumentList $SiteName, $AppPoolUser, $AppPoolPassword               
+             
+                             
+             
+            }
+        }
+
+      
+
+        
+        Write-Host -ForegroundColor green "Creating IIS Site Directory..."
+        invoke-command -session $Session -scriptblock{
+            #Site
+            $Path = "C:\Sites\" + $innerNewSiteName
+            
+           
+            if (test-path $Path)
+            {
+                write-host -ForegroundColor red "The site directory already exists"
+            }
+            else
+            {
+                Write-Host -ForegroundColor green "Creating site directory..."
+                New-Item $Path -type Directory
+                Set-Location C:\windows\System32\inetsrv
+                Write-Host -ForegroundColor green "Creating IIS site..."
+                .\appcmd.exe add site /name:$innerNewSiteName /physicalPath:$Path /bindings:http/*:80:$NewSite
+                Write-Host -ForegroundColor green "Creating AppPool..."
+                .\appcmd.exe add apppool /name:$innerNewSiteName /managedRuntimeVersion:v4.0
+               
+                
+               
+                
+               
+           
+                              
+                $AppPool = "appcmd.exe"
+                $AppPoolArguments1 = "set site /site.name:$innerNewSiteName /[path='/'].applicationPool:$innerNewSiteName"
+                $AppPoolArguments2 = "set config /section:applicationPools /[name='$innerNewSiteName'].processModel.identityType:SpecificUser /[name='$innerNewSiteName'].processModel.userName:mi\$innerAppPoolUser /[name='$innerNewSiteName'].processModel.password:$innerAppPoolPassword"
+                
+                Write-Host -ForegroundColor green "Creating Application Pool..."
+                Start-Process -FilePath $AppPool -Argumentlist $AppPoolArguments1
+             
+                Write-Host -ForegroundColor green "Setting Service Account ..."
+                Start-Process -FilePath $AppPool -Argumentlist $AppPoolArguments2
+             
+            }
+
+        }
+        
+        Write-Host -ForegroundColor green "Ending session for $server..."
+        Remove-PSSession $session
+        
+    }
+
+
